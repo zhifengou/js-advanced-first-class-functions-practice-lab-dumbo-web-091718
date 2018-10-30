4dc9d9848c8a61b990b91dd3eaf2a701fc05e415
zhifeng ou <zhifengou@hotmail.com>
2018-10-30 15:59:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 // Code your solution in this file!
-function logDriverNames(drivers){
-  drivers.forEach(function(d){
-    console.log(d.name);
-  });
-}
+// function logDriverNames(drivers){
+//   drivers.forEach(function(d){
+//     console.log(d.name);
+//   });
+// }
 
-function logDriversByHometown(drivers, ht) {
- drivers.forEach(function(d){
-   if(d.hometown===ht){
-     console.log(d.name);
-   }
- })
-}
+// function logDriversByHometown(drivers, ht) {
+// drivers.forEach(function(d){
+//   if(d.hometown===ht){
+//     console.log(d.name);
+//   }
+// })
+// }
 
-function driversByRevenue(drivers) {
-  newDrivers = [...drivers]
-  return newDrivers.sort(function (d1, d2) {
-    return d1.revenue - d2.revenue
-  })
-}
+// function driversByRevenue(drivers) {
+//   newDrivers = [...drivers]
+//   return newDrivers.sort(function (d1, d2) {
+//     return d1.revenue - d2.revenue
+//   })
+// }
 
 
-function driversByName(drivers){
-  return drivers.sort(function(d1,d2){
-    return d1.name.localeCompare(d2.name)
-  })
-}
+// function driversByName(drivers){
+//   return drivers.sort(function(d1,d2){
+//     return d1.name.localeCompare(d2.name)
+//   })
+// }
 
-function totalRevenue(drivers){
-  sum=0
-  drivers.forEach(function(d){
-    sum+=d.revenue
-  })
-  return sum;
-}
+// function totalRevenue(drivers){
+//   sum=0
+//   drivers.forEach(function(d){
+//     sum+=d.revenue
+//   })
+//   return sum;
+// }
 
-function averageRevenue(drivers){
- sum=function totalRevenue(drivers)();
- return sum/drivers.length;
-}
+// function averageRevenue(drivers){
+// sum=function totalRevenue(drivers)();
+// return sum/drivers.length;
+// }
