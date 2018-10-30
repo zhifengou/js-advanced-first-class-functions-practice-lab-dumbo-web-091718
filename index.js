dcde30722376442ff8abdf0b45b9f03273cba1ff
zhifeng ou <zhifengou@hotmail.com>
2018-10-30 16:13:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(d){
+    console.log(d.name)
+  })
+}
+
+function logDriversByHometown(drivers, ht) {
+drivers.forEach(function(d){
+  if(d.hometown===ht){
+    console.log(d.name);
+  }
+})
+}
+
+function driversByRevenue(drivers) {
+  newDrivers = [...drivers]
+  return newDrivers.sort(function (d1, d2) {
+    return d1.revenue - d2.revenue
+  })
+}
+
+
+function driversByName(drivers){
+  newDrivers = [...drivers]
+  return newDrivers.sort(function(d1,d2){
+    return d1.name.localeCompare(d2.name)
+  })
+}
+
+// function totalRevenue(drivers){
+//   sum=0
+//   drivers.forEach(function(d){
+//     sum+=d.revenue
+//   })
+//   return sum;
+// }
+function totalRevenue(drivers){
+   return drivers.reduce(function(accumulator, currentValue){
+    return accumulator + currentValue.revenue;
+  },0)
+}
+
+function averageRevenue(drivers){
+return totalRevenue(drivers)/drivers.length
+}
