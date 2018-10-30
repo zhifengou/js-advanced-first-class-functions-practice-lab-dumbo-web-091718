6817ca9ee6e84d76cad4f08c9109a48ac83168fe
zhifeng ou <zhifengou@hotmail.com>
2018-10-30 15:48:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 const logDriverNames =function(drivers){
   drivers.forEach(function(d){
-    console.log(d.name)
-  })
-}
+    console.log(d.name);
+  });
+};
 
 function logDriversByHometown(drivers, ht) {
  drivers.forEach(function(d){
