768356c7a1e6f5722fac18a70d81c53688c7e522
zhifeng ou <zhifengou@hotmail.com>
2018-10-30 16:04:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(d){
+    console.log(d.name)
+  })
+}
 // function logDriverNames(drivers){
-//   drivers.forEach(function(d){
-//     console.log(d.name)
+//   drivers.forEach(function(element) {
+//     console.log(element['name']);
+//   });
+// }
+
+// function logDriversByHometown(drivers, ht) {
+// drivers.forEach(function(d){
+//   if(d.hometown===ht){
+//     console.log(d.name);
+//   }
+// })
+// }
+
+// function driversByRevenue(drivers) {
+//   newDrivers = [...drivers]
+//   return newDrivers.sort(function (d1, d2) {
+//     return d1.revenue - d2.revenue
 //   })
 // }
-function logDriverNames(drivers){
-  drivers.forEach(function(element) {
-    console.log(element['name']);
-  });
-}
-
-function logDriversByHometown(drivers, ht) {
-drivers.forEach(function(d){
-  if(d.hometown===ht){
-    console.log(d.name);
-  }
-})
-}
-
-function driversByRevenue(drivers) {
-  newDrivers = [...drivers]
-  return newDrivers.sort(function (d1, d2) {
-    return d1.revenue - d2.revenue
-  })
-}
 
 
-function driversByName(drivers){
-  return drivers.sort(function(d1,d2){
-    return d1.name.localeCompare(d2.name)
-  })
-}
+// function driversByName(drivers){
+//   return drivers.sort(function(d1,d2){
+//     return d1.name.localeCompare(d2.name)
+//   })
+// }
 
-function totalRevenue(drivers){
-  sum=0
-  drivers.forEach(function(d){
-    sum+=d.revenue
-  })
-  return sum;
-}
+// function totalRevenue(drivers){
+//   sum=0
+//   drivers.forEach(function(d){
+//     sum+=d.revenue
+//   })
+//   return sum;
+// }
 
-function averageRevenue(drivers){
-sum=function totalRevenue(drivers)();
-return sum/drivers.length;
-}
+// function averageRevenue(drivers){
+// sum=function totalRevenue(drivers)();
+// return sum/drivers.length;
+// }
+
+
 // function logDriverNames(drivers){
 //   drivers.forEach(function(element) {
 //     console.log(element['name']);
