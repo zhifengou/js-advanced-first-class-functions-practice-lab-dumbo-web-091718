1b5f091509a2aa2dcdcd111a360ca5bd5ed188b8
zhifeng ou <zhifengou@hotmail.com>
2018-10-30 15:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,3 +39,41 @@
 // sum=function totalRevenue(drivers)();
 // return sum/drivers.length;
 // }
+function logDriverNames(drivers) {
+  drivers.reduce((agg, el, i, arr) => console.log(el.name), [])
+}
+
+function logDriversByHometown(drivers, keyword) {
+  drivers.reduce((agg, el, i, arr) => {
+    if (el.hometown === keyword) {
+      console.log(el.name)
+    }
+  }, [])
+}
+
+function driversByRevenue(drivers) {
+  newDrivers = [...drivers]
+  return newDrivers.sort((a, b) => {
+    return (a.revenue - b.revenue)
+  })
+}
+
+function driversByName(drivers) {
+  newDrivers = [...drivers]
+  return newDrivers.sort((a, b) => {
+    return a.name.localeCompare(b.name)
+  })
+}
+
+function totalRevenue(drivers) {
+  return drivers.reduce((agg, el, i, arr) => {
+    return agg + el.revenue
+  }, 0)
+}
+
+function averageRevenue(drivers) {
+  const sum = drivers.reduce((agg, el, i, arr) => {
+    return agg + el.revenue
+  }, 0)
+  return sum / drivers.length
+}
